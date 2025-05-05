8106ced87439805e9dad74795dddb136ee52d419
SecreShall <164751004+SecreShall@users.noreply.github.com>
2025-05-05 17:52:52 +0800

    add

---
+++ DAG.py
@@ -0,0 +1,39 @@
+#import libraries
+from airflow.operators.python import PythonOperator
+from airflow import DAG
+from datetime import timedelta
+from ETL import extract, transform, load
+
+default_args={
+     'owner': 'Cley',
+     'start_date': timedelta(days=1),
+     'retries': 1,
+     'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+     dag_id='weather_etl',
+     default_args=default_args,
+     description='Weather Data Pipeline',
+     schedule=timedelta(days=1),
+)
+
+extract_task = PythonOperator(
+    task_id='extract',
+    python_callable=extract,
+    dag=dag,
+)
+
+transform_task = PythonOperator(
+    task_id='transform',
+    python_callable=transform,
+    dag=dag,
+)
+
+load_task = PythonOperator(
+    task_id='load',
+    python_callable=load,
+    dag=dag,
+)
+
+extract_task >> transform_task >> load_task
---
+++ ETL.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+API_KEY = '597bd834ee9da20bf2ef005a3d677718'
+
+
+def extract():
+    print('yameteee')
+
+
+def transform():
+	pass
+
+
+def load():
+    pass
